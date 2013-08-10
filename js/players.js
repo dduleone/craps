01e5cf06fa8d468f386f6bae82e7fe63d5b879ea
Matt DuLeone <matt@duleone.com>
2013-08-10 18:33:54 -0400

    trying to fix the modal screen app bug

--- js/players.js
+++ js/players.js
@@ -36,27 +36,14 @@ $.extend(PlayerManager, {
   },
   updatePlayerArea: function(){
     
-    //var name = $('#name');
     var bank = $('#bank');
-    //name.empty();
-    //bank.empty();
-    //name.html(this.players[0].player.name);
     $('#settingsName').val(this.players[0].player.name)
-    //name.width($('#board').width()/(0.7)*(0.29));
-    //name.css({top: '5px', left: $('#board').width()/(0.7)*(0.0025)});
     if($('#modalButtons').height() > 60){
       $('#modalWindow').height($('#modal').height() - 95);
     }else{
       $('#modalWindow').height($('#modal').height() - 65);
     }
     $('#tutorialInstruction').css({'top': ($('#generalButtons').offset().top + $('#generalButtons').height())});
-    //$('#playerArea').css({left: '0px'});
-    //var bankTitle = $(document.createElement('p'));
-    //bankTitle.html('<center>Bank</center>');
-    //if(bank.children().length == 0){
-    //  bank.append(bankTitle);
-    //}
-    //var avail = $(document.createElement('div'));
     var totalBets = 0;
     for(x in _CRAPS.dealer.betManager.bets){
       totalBets += _CRAPS.dealer.betManager.bets[x].bet.value;
@@ -76,22 +63,8 @@ $.extend(PlayerManager, {
     }else{
       total.attr('class', 'winning');
     }
-    //bank.append(avail);
-    //bank.append(inPlay);
-    //bank.append(total);
-    
     $('#bank').css({top: 0, margin: '0px', border: '0px'});
-    //$('#bankTitle').css({top: ($('#bank').offset().top), margin: '0px', border: '0px'});
-    
-    //bank.height(Math.max($('#availBank').offset().top, $('#inPlay').offset().top, $('#totBank').offset().top) + Math.max($('#availBank').height(), $('#inPlay').height(), $('#totBank').height()));
-    
-    //$('#buffer').css({top: $('#playerArea').height(), left: $('#board').width()/(0.7)*(0.0025)});
-    //$('#buffer').height(($('#board').height() - $('#playerArea').height())/2);
-    //$('#betListing').css('top', ($('#buffer').offset().top + $('#buffer').height() + 2));
-    //$('#betListing').css('top', ($('#bank').height()));
-    $('#bank').height(Math.max($('#availBank').offset().top, $('#inPlay').offset().top, $('#totBank').offset().top) + Math.max($('#availBank').height(), $('#inPlay').height(), $('#totBank').height()));
-    //$('#betListing').height($('#board').height() - $('#playerArea').height() - $('#buffer').height() + $(board).height()*(3/7));
-    //$('#closeBets').css('bottom', ($('#betListWindow').height() - $('#betListing').height() - $('#playerArea').height() - $('#closeBets').height()));
+    //$('#bank').height(Math.max($('#availBank').offset().top, $('#inPlay').offset().top, $('#totBank').offset().top) + Math.max($('#availBank').height(), $('#inPlay').height(), $('#totBank').height()));
     $('#betListWindow').height($('#betListBG').height() - 35);
     $('#betResultsWindow').height($('#betResultsBG').height() - 35);
     
