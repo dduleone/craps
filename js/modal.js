c23679ef03070cd18c1cef0f01b45f0523046991
Matt DuLeone <matt@duleone.com>
2013-08-22 10:21:45 -0400

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -173,7 +173,7 @@ function resizeModal(){
             $('#tutorialButton').width() + 
             $('#aboutButton').width() + 
             115);
-  $('#modalWindow').height(mod.height() - $('#board').height()*(0.02) - 35);
+  $('#modalWindow').height(mod.height()*(1.005) - $('#board').height()*(0.02) - 35);
   var rules = $('#rules');
   var rulesHidden = (rules.css('visibility')== 'hidden' || rules.css('display') == 'none')
   if(rulesHidden){
